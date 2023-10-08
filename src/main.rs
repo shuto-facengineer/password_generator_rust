21c224a31982e429c7430e0b42735ccdd6a69286
Takuya-Shuto-engineer <shuto.takuya.sy@alumni.tsukuba.ac.jp>
2023-10-09 00:24:26 +0900

    とりあえず動くもの

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+extern crate rand;
+use rand::{thread_rng, Rng};
+use std::env;
+
+const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
+abcdefghijklmnopqrstuvwxyz\
+0123456789)(*&^%$#@!~";
+const DEFAULT_LENGTH: usize = 12;
+
+fn generate_password(length: usize) -> String {
+    let mut rng = thread_rng();
+
+    let password: String = (0..length)
+        .map(|_| {
+            let idx = rng.gen_range(0..CHARSET.len());
+            CHARSET[idx] as char
+        }).collect();
+    
+    password
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let length = match args.get(1) {
+        Some(l) => match l.parse::<usize>() { 
+            Ok(parsed_length) => parsed_length,
+            Err(_) => DEFAULT_LENGTH, // usizeにパースできないケース（数値ではない等）はデフォルト値を使用
+        },
+        None => DEFAULT_LENGTH, // 引数がなければデフォルト値を使用
+    };
+
+    println!("Generated Password: {}", generate_password(length));
 }
