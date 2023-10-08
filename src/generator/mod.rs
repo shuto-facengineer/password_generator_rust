a2f0623d92ceef52181cb581fae67d6572a584bb
Takuya-Shuto-engineer <shuto.takuya.sy@alumni.tsukuba.ac.jp>
2023-10-09 01:49:11 +0900

    テストの追加

--- src/generator/mod.rs
+++ src/generator/mod.rs
@@ -16,3 +16,24 @@ pub fn generate_password(length: usize) -> String {
     
     password
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_generate_password_length() { // パスワードのサイズが指定した通りになること
+        let length = 10;
+        let password = generate_password(length);
+        assert_eq!(password.len(), length);
+    }
+
+    #[test]
+    fn test_generate_password_charset() { // 想定通りのキャラクターセットを利用すること
+        let length = 1000;
+        let password = generate_password(length);
+        for ch in password.chars() {
+            assert!(CHARSET.contains(&(ch as u8)));
+        }
+    }
+}
